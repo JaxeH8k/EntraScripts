b16f8cbf2cafbeab079ccf27b16c11159f819fed
Jaxe H8k <jh@dangeroustoysforkids.com>
2025-01-08 10:19:36 -0800

    fixed device filter

--- Groups/Get-GroupMembers.ps1
+++ Groups/Get-GroupMembers.ps1
@@ -3,18 +3,22 @@ $groupIds = @(
 )
 
 $graphApiEndpoint = 'https://graph.microsoft.com/v1.0/groups'
+$selectAttribs = 'displayName,operatingSystem,managementType,approximateLastSignInDateTime,'
 $rpt = @()
 foreach($id in $groupIds){
     # only process $id if it contains a 1 and only 1 comma in the string.
     if($id.IndexOf(',') -ne -1 -and $id.IndexOf(',') -eq $id.LastIndexOf(',')){
-        $graphRequestUrl = $graphApiEndpoint, "{$(($id -split ',')[0])}", 'members' -join '/'
+        $graphRequestUrl = $graphApiEndpoint, "{$(($id -split ',')[0])}", 'members', "microsoft.graph.device?`$select=$($selectAttribs)" -join '/'
         $request = Invoke-MgGraphRequest -Uri $graphRequestUrl -Method GET
-        if($request.count -gt 0){
-            foreach($i in $request.Values){
+        if($request.Values.count -gt 0){
+            foreach($i in $request.Value){
                 $rpt += [PSCustomObject]@{
                     GroupId = ($id.Split(','))[0]
                     GroupName = ($id.Split(','))[1]
-                    DisplayName = $
+                    displayName = $i.displayName
+                    managementType = $i.managementType
+                    operatingSystem = $i.operatingSystem
+                    approximateLastSignInDateTime = $i.approximateLastSignInDateTime
                 }
             }
         }
