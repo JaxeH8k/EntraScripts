b588aea6ed713e81a38403040a18087efabc8379
Jaxe H8k <jh@dangeroustoysforkids.com>
2025-01-08 11:47:19 -0800

    added support for NextLink processing (large group support)

--- Groups/Get-GroupMembers.ps1
+++ Groups/Get-GroupMembers.ps1
@@ -9,18 +9,28 @@ foreach ($id in $groupIds) {
     # only process $id if it contains a 1 and only 1 comma in the string.
     if ($id.IndexOf(',') -ne -1 -and $id.IndexOf(',') -eq $id.LastIndexOf(',')) {
         $graphRequestUrl = $graphApiEndpoint, "{$(($id -split ',')[0])}", 'members', "microsoft.graph.device?`$select=$($selectAttribs)" -join '/'
-        $request = Invoke-MgGraphRequest -Uri $graphRequestUrl -Method GET
-        if ($request.Values.count -gt 0) {
-            foreach ($i in $request.Value) {
-                $rpt += [PSCustomObject]@{
-                    GroupId                       = ($id.Split(','))[0]
-                    GroupName                     = ($id.Split(','))[1]
-                    displayName                   = $i.displayName
-                    managementType                = $i.managementType
-                    operatingSystem               = $i.operatingSystem
-                    approximateLastSignInDateTime = $i.approximateLastSignInDateTime
+        # using while loop for processing NextLinks
+        do {
+            $request = Invoke-MgGraphRequest -Uri $graphRequestUrl -Method GET
+            if ($request.Values.count -gt 0) {
+                foreach ($i in $request.Value) {
+                    $rpt += [PSCustomObject]@{
+                        GroupId                       = ($id.Split(','))[0]
+                        GroupName                     = ($id.Split(','))[1]
+                        displayName                   = $i.displayName
+                        managementType                = $i.managementType
+                        operatingSystem               = $i.operatingSystem
+                        approximateLastSignInDateTime = $i.approximateLastSignInDateTime
+                    }
                 }
             }
-        }
+            #
+            if ($request.PSObject.Properties.Name -contains '@odata.nextLink') {
+                $graphRequestUrl = $request.'@odata.nextLink'
+            } else {
+                $graphRequestUrl = $null
+            }
+
+        } while ( $null -notlike $graphRequestUrl)
     }
 }
